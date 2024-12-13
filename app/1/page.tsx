e1c099c704b07fedce4ec98eccc336c06a9cd738
Jen Looper <jen.looper@gmail.com>
2024-12-13 18:14:21 -0500

    routes

--- app/1/page.tsx
+++ app/1/page.tsx
@@ -72,18 +72,7 @@ export default function ScrollytellingComponent() {
 
   return (
     <div className="page-wrapper">
-      <header className="main-header">
-        <nav className="main-nav bg-green-800">
-          <div className="logo">Snappy Squirrel ScrollyBooks</div>
-          <ul className="main-nav-links">
-          <ul className="main-nav-links">
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/about">About</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
-          </ul>
-          </ul>
-        </nav>
-      </header>
+   
 
       <div className="content-wrapper">
         <div className="scroll-wrapper">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playwrite_FR_Moderne } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const font = Playwrite_FR_Moderne({
@@ -23,6 +24,18 @@ export default function RootLayout({
         className={`${font.variable} antialiased`}
       >
        <div className="overflow-auto">
+       <header className="main-header">
+        <nav className="main-nav bg-green-800">
+          <div className="logo">Snappy Squirrel ScrollyBooks</div>
+          <ul className="main-nav-links">
+          <ul className="main-nav-links">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/contact">Contact</Link></li>
+          </ul>
+          </ul>
+        </nav>
+      </header>
           {children}
         </div>
       </body>
