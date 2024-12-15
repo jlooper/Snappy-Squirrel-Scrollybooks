3b5c35bc8e926d325ed51830ac5eaa3305106bd2
Jen Looper <jen.looper@gmail.com>
2024-12-14 21:51:09 -0500

    fixin' the build

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,18 @@
 // app/not-found.tsx
+import Link from 'next/link';
+
 export default function NotFound() {
     return (
       <div className="min-h-screen flex items-center justify-center">
         <div className="text-center text-white p-20 bg-green-800">
           <h1 className="text-4xl font-bold mb-4">Page Not Found</h1>
-          <p className="text-xl mb-6">Oops! Snappy couldn't find that story.</p>
-          <a 
+          <p className="text-xl mb-6">Oops! Snappy could&lsquo;t find that story.</p>
+          <Link 
             href="/" 
             className="inline-block bg-green-600 px-6 py-3 rounded-lg hover:bg-green-700 transition-colors"
           >
             Go Back Home
-          </a>
+          </Link>
         </div>
       </div>
     );
