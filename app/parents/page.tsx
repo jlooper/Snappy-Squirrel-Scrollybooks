7e00c26ccd3a2a82ace2b71bb2561e47e12eb9ed
Jen Looper <jen.looper@gmail.com>
2024-12-27 11:49:14 -0500

    fixing unescaped /'

--- app/parents/page.tsx
+++ app/parents/page.tsx
@@ -4,7 +4,7 @@ export default function Parents() {
     return (
       <div className="container mx-auto p-4 mt-20">
       <h1 className="text-3xl font-bold mb-6">Parent Corner</h1>
-      <p className="text-2xl">Welcome to the Parent Corner! Here, you'll find resources and tips to help you teach your child about money and financial literacy. Click on a card to learn more!</p>
+      <p className="text-2xl">Welcome to the Parent Corner! Here, you can find resources and tips to help you teach your child about money and financial literacy. Click on a card to learn more!</p>
       </div>
     );
   }
